6bca58cff6b6583df98320c0c54e72149541d720
arsvechkarev <arsvechkarev@gmail.com>
2021-03-23 11:35:18 +0300

    Fixing release build

--- app/src/release/java/com/arsvechkarev/coronka/CoronkaApplication.kt
+++ app/src/release/java/com/arsvechkarev/coronka/CoronkaApplication.kt
@@ -2,14 +2,12 @@ package com.arsvechkarev.coronka
 
 import android.util.Log
 import timber.log.Timber
-import com.arsvechkarev.common.CoreDiComponent
 
 class CoronkaApplication : CoronkaBaseApplication() {
   
   override fun onCreate() {
     super.onCreate()
     Timber.plant(ReleaseTree)
-    CoreDiComponent.initDefault(applicationContext)
   }
   
   object ReleaseTree : Timber.Tree() {
